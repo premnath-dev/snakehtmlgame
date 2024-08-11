7fa392e82540e96d33dd3242950a3944cf60c210
premnath <prem.netaji.super@gmail.com>
2024-08-11 22:27:48 +0530

    a snake game

---
+++ game.js
@@ -0,0 +1,16 @@
+export function setupBoard(element) {
+  return element
+}
+export function setupInstructionText(element) {
+    return element
+}
+export function setupLogo(element) {
+    return element
+}
+export function setupScore(element) {
+    return element
+}
+export function setupHighScoreText(element) {
+    return element
+}
+
